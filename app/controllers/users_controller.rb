a2a061dcc4f9ce1c94899d6725c0a22e4ae0513f
Shu Kumata <shukuma11.sns@gmail.com>
2017-06-23 03:58:53 +0900

    make model of Skill and change layouts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
     def show
         @user = User.find(params[:id])
+        @skills = Skill.where(user_id: params[:id]).group(:name).count
     end
     def new
         @user = User.new
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170622041154) do
+ActiveRecord::Schema.define(version: 20170622174850) do
+
+  create_table "skills", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id"
+    t.integer "from_user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
