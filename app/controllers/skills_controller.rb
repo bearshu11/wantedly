ab1b21a5275e846573e70985d20233217b79a735
Shu Kumata <shukuma11.sns@gmail.com>
2017-06-23 07:17:49 +0900

    if you have ever push the +1 button, you cannot push it

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,13 +1,20 @@
 class SkillsController < ApplicationController
     def create
-        @skill = Skill.new(name: params[:skill][:name], 
-                           user_id: params[:skill][:user_id],
-                           from_user_id: session[:user_id],
-                           )
-        @user = User.find(@skill.user_id)
+        stored_skills = Skill.where(user_id: params[:skill][:user_id],
+                                    from_user_id: session[:user_id]
+                                   )
+        @user = User.find(params[:skill][:user_id])
+        if stored_skills.empty?
+            @skill = Skill.new(name: params[:skill][:name], 
+                               user_id: params[:skill][:user_id],
+                               from_user_id: session[:user_id],
+                               )
 
-        if @skill.save
-            redirect_to user_url(@user)
+            if @skill.save
+                redirect_to user_url(@user)
+            else
+                redirect_to user_url(@user)
+            end
         else
             redirect_to user_url(@user)
         end
