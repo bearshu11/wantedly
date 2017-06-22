4e41c699978ab5b764c711b3b49f8b383a99e09a
Shu Kumata <shukuma11.sns@gmail.com>
2017-06-23 05:38:09 +0900

    you can add skill from page

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -1,4 +1,14 @@
 class SkillsController < ApplicationController
-  def create
-  end
+    def create
+        @skill = Skill.new(name: params[:skill][:name], 
+                           user_id: params[:skill][:user_id],
+                           from_user_id: session[:user_id],
+                           )
+        @user = User.find(@skill.user_id)
+        if @skill.save
+            redirect_to user_url(@user)
+        else
+            redirect_to user_url(@user)
+        end
+    end
 end
