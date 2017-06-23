eeb62ca156f99491b5a41864b99d22ee86fdf769
Shu Kumata <shukuma11.sns@gmail.com>
2017-06-23 21:14:27 +0900

    make index of users and restriction of access

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+    before_action :logged_in_user, only: [:show, :create, :index]
+    
+    def index
+        @users = User.all
+    end
+
     def show
         @user = User.find(params[:id])
         skills = Skill.where(user_id: params[:id]).group(:name).count
@@ -23,4 +29,11 @@ class UsersController < ApplicationController
         def user_params
               params.require(:user).permit(:name, :email, :password, :password_confirmation)
         end
+        
+        def logged_in_user
+            unless logged_in?
+             redirect_to login_url
+            end
+        end
+
 end
